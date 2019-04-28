52534c48ca69c7b9dc989a582aa7b8701fab55f8
dj_yoko <yokochan.recurrent@gmail.com>
2019-04-28 22:43:30 +0900

    describe Index page as React component grammer.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
-const Index = () => (
-  <div>
-    <p>Hello Next.js</p>
-  </div>
-)
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import _JSXStyle from 'styled-jsx/style';
+import css from 'styled-jsx/css';
+
+export class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        this is index page.
+      </div>
+    );
+  }
+}
 
 export default Index
