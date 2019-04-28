425f8b584f8eeaf282be655da5d35674a56225fa
dj_yoko <yokochan.recurrent@gmail.com>
2019-04-29 01:01:40 +0900

    add commonHead component, load Bootstrap css

---
+++ src/component/commonHead.js
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+
+export class CommonHead extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <Head>
+                    <title>{this.props.title}</title>
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                    <link rel="stylesheet" href="/static/css/common.css"></link>
+                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"></link>
+                </Head>
+            </div>
+        );
+    }
+}
+
+
+export default CommonHead;
