22d5b0931d6735eb951b1d0348a7f38cf0e34c1a
dj_yoko <yokochan.recurrent@gmail.com>
2019-04-29 23:40:06 +0900

    add Static text data for contents

---
+++ src/data/Areas.js
@@ -0,0 +1,14 @@
+const Areas = [
+    {
+        'path': 'MinatoMirai21',
+        'description': 'Minato Mirai 21 (みなとみらい21 Minato Mirai Nijūichi), often known as simply Minato Mirai and abbreviated as MM, is the central business district of Yokohama, Japan. Initially developed in the 1980s, Minato Mirai 21 was designed as a large master-planned development and new urban center planned to connect Yokohama\'s traditionally important areas and commercial centers of Kannai and the Yokohama Station area.',
+        'title': 'Minato Mirai 21'
+    },
+    {
+        'path': 'RedBrickWarehouses',
+        'description': 'The Yokohama Red Brick Warehouse (横浜赤レンガ倉庫 Yokohama Akarenga Sōko) is a historical building that is used as a complex that includes a shopping mall, banquet hall, and event venues.',
+        'title': 'Red Brick Warehouses'
+    }
+];
+
+export default Areas
