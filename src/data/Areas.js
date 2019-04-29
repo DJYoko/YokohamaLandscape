d3e33bd60e9341a538cfd358e76ff3f2d65692f7
dj_yoko <yokochan.recurrent@gmail.com>
2019-04-30 00:13:55 +0900

    add static text datas

--- src/data/Areas.js
+++ src/data/Areas.js
@@ -8,7 +8,20 @@ const Areas = [
         'path': 'RedBrickWarehouses',
         'description': 'The Yokohama Red Brick Warehouse (横浜赤レンガ倉庫 Yokohama Akarenga Sōko) is a historical building that is used as a complex that includes a shopping mall, banquet hall, and event venues.',
         'title': 'Red Brick Warehouses'
-    }
+    },
+    
+    {
+        'path': 'YokohamaStation',
+        'description': 'Yokohama Station (横浜駅 Yokohama-eki) is a major interchange railway station in Nishi-ku, Yokohama, Japan. It is the busiest station in Kanagawa Prefecture and the fifth-busiest in the world as of 2013,[1] serving 760 million passengers a year.',
+        'title': 'Yokohama Station'
+    },
+    
+    {
+        'path': 'YokohamaThreeTowers',
+        'description': 'Yokohama three towers are a group of historical towers at the Port of Yokohama. They have been given the nicknames The King, The Queen and The Jack. The best view of the three towers is considered to be from Ōsanbashi Pier.',
+        'title': 'Yokohama Three Towers'
+    },
+
 ];
 
 export default Areas
