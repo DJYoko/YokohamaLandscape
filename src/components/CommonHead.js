554e2aeb13e6eb5453e718a4e4e606793cf5e93b
dj_yoko <yokochan.recurrent@gmail.com>
2019-04-29 23:39:17 +0900

    reduce nest on CommonHead

--- src/components/CommonHead.js
+++ src/components/CommonHead.js
@@ -6,14 +6,12 @@ export class CommonHead extends React.Component {
     }
     render() {
         return (
-            <div>
-                <Head>
-                    <title>{this.props.title}</title>
-                    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                    <link rel="stylesheet" href="/static/css/common.css"></link>
-                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"></link>
-                </Head>
-            </div>
+            <Head>
+                <title>{this.props.title}</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"></link>
+                <link rel="stylesheet" href="/static/css/common.css"></link>
+            </Head>
         );
     }
 }
