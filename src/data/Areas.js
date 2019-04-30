acd33355442c17b3a2ea09f9bf28ce7abdfe9ce1
yuichiyokoyama <yuichi.yokoyama@concores.co.jp>
2019-04-30 14:37:59 +0900

    add static data

--- src/data/Areas.js
+++ src/data/Areas.js
@@ -1,5 +1,11 @@
 const Areas = [
     {
+        'path': 'YokohamaStation',
+        'description': 'Yokohama Station (横浜駅 Yokohama-eki) is a major interchange railway station in Nishi-ku, Yokohama, Japan. It is the busiest station in Kanagawa Prefecture and the fifth-busiest in the world as of 2013,[1] serving 760 million passengers a year.',
+        'title': 'Yokohama Station',
+        'img': 'photo-1543932950-76d6b4a4d557.jpg',
+    },
+    {
         'path': 'MinatoMirai21',
         'description': 'Minato Mirai 21 (みなとみらい21 Minato Mirai Nijūichi), often known as simply Minato Mirai and abbreviated as MM, is the central business district of Yokohama, Japan. Initially developed in the 1980s, Minato Mirai 21 was designed as a large master-planned development and new urban center planned to connect Yokohama\'s traditionally important areas and commercial centers of Kannai and the Yokohama Station area.',
         'title': 'Minato Mirai 21',
@@ -11,21 +17,18 @@ const Areas = [
         'title': 'Red Brick Warehouses',
         'img': 'C778_rtsaretaakarenga_TP_V.jpg',
     },
-
-    {
-        'path': 'YokohamaStation',
-        'description': 'Yokohama Station (横浜駅 Yokohama-eki) is a major interchange railway station in Nishi-ku, Yokohama, Japan. It is the busiest station in Kanagawa Prefecture and the fifth-busiest in the world as of 2013,[1] serving 760 million passengers a year.',
-        'title': 'Yokohama Station',
-        'img': 'photo-1543932950-76d6b4a4d557.jpg',
-    },
-
     {
         'path': 'YokohamaPortOpeningMemorialHall',
         'description': 'Yokohama City Port Opening Memorial Hall is a historical building located in the Kannai area of Naka Ward, Yokohama City, Kanagawa Prefecture. Important cultural property of the country.',
         'title': 'Yokohama Port Opening Memorial Hall',
         'img': 'Yokohama_Port_Opening_Memorial_Hall.jpg',
     },
-
+    {
+        'path': 'YokohamaChinatown',
+        'description': 'Yokohama Chinatown (Japanese: 横浜中華街, yokohama chūkagai; Simplified Chinese: 横滨中华街; Traditional Chinese: 橫濱中華街; Pinyin: Héngbīn Zhōnghuá Jiē; Cantonese Jyutping: Waang4 ban1 zung1 waa4 gaai1) is located in Yokohama, Japan, which is located just south of Tokyo. ',
+        'title': 'Yokohama Chinatown',
+        'img': 'photo-1530569243640-22bea3e23e1b.jpg',
+    },
 ];
 
 export default Areas
