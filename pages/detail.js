91beb4ebccbd2f3b2ac4b7bb1de9831ba87b7735
yuichiyokoyama <yuichi.yokoyama@concores.co.jp>
2019-04-30 13:35:23 +0900

    add Back Link for detail pages. (it links to index page)

--- pages/detail.js
+++ pages/detail.js
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 import _JSXStyle from 'styled-jsx/style';
 import css from 'styled-jsx/css';
 import Areas from '../src/data/Areas';
+import Link from 'next/link';
 import CommonHead from '../src/components/CommonHead';
-import Link from 'next/link'
 
 export class detail extends React.Component {
   constructor(props) {
@@ -13,55 +13,55 @@ export class detail extends React.Component {
     this.state = {};
   }
   static getInitialProps({ query }) {
-    return {query};
+    return { query };
   }
   render() {
     const areaData = this.getAreaData(this.props.query.name);
     if (areaData !== null) {
       this.setAreaData(areaData);
-      return this.renderDefault ();
+      return this.renderDefault();
     } else {
-      return this.renderError ();
+      return this.renderError();
     }
   }
-  getAreaData (path) {
+  getAreaData(path) {
     const filteredAreas = Areas.filter((area) => {
       return (area.path === path);
     });
     return (filteredAreas.length > 0) ? filteredAreas[0] : null;
   }
-  setAreaData (areaData) {
+  setAreaData(areaData) {
     this.state.path = areaData.path;
     this.state.description = areaData.description;
     this.state.title = areaData.title;
     this.state.img = areaData.img;
     return this.state;
   }
-  renderDefault () {
+  renderDefault() {
     return (
-      <div className="pageRoot" style={{backgroundImage : this.getBackgroundImageStyle()}}>
-       <CommonHead></CommonHead>
-       <div className="container">
-        
-        <div className="text">
-          <h1>{this.state.title}</h1>
-          <p>{this.state.description}</p>
+      <div className="pageRoot" style={{ backgroundImage: this.getBackgroundImageStyle() }}>
+        <CommonHead></CommonHead>
+        <div className="container">
+          <BackLink></BackLink>
+          <div className="text">
+            <h1>{this.state.title}</h1>
+            <p>{this.state.description}</p>
+          </div>
         </div>
-        </div>
-          <style jsx>{styles}</style>
+        <style jsx>{styles}</style>
       </div>
     )
   }
-  renderError () {
+  renderError() {
     return (
       <div>
-      <CommonHead></CommonHead>
+        <CommonHead></CommonHead>
         <p>Sorry page data is not found.</p>
       </div>
     )
   }
-  getBackgroundImageStyle () {
-      return 'url(/static/img/background/' + this.state.img + ')';
+  getBackgroundImageStyle() {
+    return 'url(/static/img/background/' + this.state.img + ')';
   }
 }
 
@@ -75,6 +75,7 @@ const styles = css`
   padding-top: 24px;
 }
 .text {
+  margin-top: 12px;
   padding: 24px;
   background:rgba(0,0,0,0.25);
 }
---
+++ src/components/BackLink.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import _JSXStyle from 'styled-jsx/style';
+import css from 'styled-jsx/css';
+import Link from 'next/link';
+
+export class BackLink extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="back-link-area">
+                <Link href={{
+                    pathname: '/'
+                }}>
+                    <a className="back-link">
+                        <span className="glyphicon glyphicon-arrow-left" aria-hidden="true"></span>
+                        Back
+                </a>
+                </Link>
+                <style jsx>{styles}</style>
+            </div>
+        );
+    }
+}
+
+const styles = css`
+
+    .back-link-area{
+        display:inline-block;
+    }
+    .back-link {
+        color: #d9d9d9;
+        background: rgba(0,0,0,0.25);
+        display:block;
+        padding: 8px;
+        text-align:center;
+        border-radius: 4px;
+    }
+
+    .back-link:hover {
+        text-decoration:none;
+        color: #262626;
+        background:rgba(255,255,255,0.75);
+    }
+
+    .glyphicon {
+        margin-right: 4px;
+    }
+`;
+
+
+export default BackLink;
