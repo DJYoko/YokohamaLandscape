14bd21bfbd544033b40019b6c85c3990061c88a8
yuichiyokoyama <yuichi.yokoyama@concores.co.jp>
2019-04-30 12:56:52 +0900

    add static datas
    add static datas

--- pages/detail.js
+++ pages/detail.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import _JSXStyle from 'styled-jsx/style';
 import css from 'styled-jsx/css';
 import Areas from '../src/data/Areas';
+import CommonHead from '../src/components/CommonHead';
 import Link from 'next/link'
 
 export class detail extends React.Component {
@@ -33,22 +34,50 @@ export class detail extends React.Component {
     this.state.path = areaData.path;
     this.state.description = areaData.description;
     this.state.title = areaData.title;
+    this.state.img = areaData.img;
     return this.state;
   }
   renderDefault () {
     return (
-      <div>
-        <p>{this.state.title}</p>
-        <p>{this.state.description}</p>
+      <div className="pageRoot" style={{backgroundImage : this.getBackgroundImageStyle()}}>
+       <CommonHead></CommonHead>
+       <div className="container">
+        
+        <div className="text">
+          <h1>{this.state.title}</h1>
+          <p>{this.state.description}</p>
+        </div>
+        </div>
+          <style jsx>{styles}</style>
       </div>
     )
   }
   renderError () {
     return (
       <div>
+      <CommonHead></CommonHead>
         <p>Sorry page data is not found.</p>
       </div>
     )
   }
+  getBackgroundImageStyle () {
+      return 'url(/static/img/background/' + this.state.img + ')';
+  }
 }
+
+const styles = css`
+.pageRoot {
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+}
+.container {
+  padding-top: 24px;
+}
+.text {
+  padding: 24px;
+  background:rgba(0,0,0,0.25);
+}
+`;
+
 export default detail;
--- src/data/Areas.js
+++ src/data/Areas.js
@@ -2,24 +2,28 @@ const Areas = [
     {
         'path': 'MinatoMirai21',
         'description': 'Minato Mirai 21 (みなとみらい21 Minato Mirai Nijūichi), often known as simply Minato Mirai and abbreviated as MM, is the central business district of Yokohama, Japan. Initially developed in the 1980s, Minato Mirai 21 was designed as a large master-planned development and new urban center planned to connect Yokohama\'s traditionally important areas and commercial centers of Kannai and the Yokohama Station area.',
-        'title': 'Minato Mirai 21'
+        'title': 'Minato Mirai 21',
+        'img': 'DSCF5767_TP_V.jpg',
     },
     {
         'path': 'RedBrickWarehouses',
         'description': 'The Yokohama Red Brick Warehouse (横浜赤レンガ倉庫 Yokohama Akarenga Sōko) is a historical building that is used as a complex that includes a shopping mall, banquet hall, and event venues.',
-        'title': 'Red Brick Warehouses'
+        'title': 'Red Brick Warehouses',
+        'img': 'C778_rtsaretaakarenga_TP_V.jpg',
     },
-    
+
     {
         'path': 'YokohamaStation',
         'description': 'Yokohama Station (横浜駅 Yokohama-eki) is a major interchange railway station in Nishi-ku, Yokohama, Japan. It is the busiest station in Kanagawa Prefecture and the fifth-busiest in the world as of 2013,[1] serving 760 million passengers a year.',
-        'title': 'Yokohama Station'
+        'title': 'Yokohama Station',
+        'img': 'photo-1543932950-76d6b4a4d557.jpg',
     },
-    
+
     {
-        'path': 'YokohamaThreeTowers',
-        'description': 'Yokohama three towers are a group of historical towers at the Port of Yokohama. They have been given the nicknames The King, The Queen and The Jack. The best view of the three towers is considered to be from Ōsanbashi Pier.',
-        'title': 'Yokohama Three Towers'
+        'path': 'YokohamaPortOpeningMemorialHall',
+        'description': 'Yokohama City Port Opening Memorial Hall is a historical building located in the Kannai area of Naka Ward, Yokohama City, Kanagawa Prefecture. Important cultural property of the country.',
+        'title': 'Yokohama Port Opening Memorial Hall',
+        'img': 'Yokohama_Port_Opening_Memorial_Hall.jpg',
     },
 
 ];
