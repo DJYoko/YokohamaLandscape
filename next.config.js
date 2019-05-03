8014c75e1874bcbd8a24f730efdaa0ce80996641
dj_yoko <yokochan.recurrent@gmail.com>
2019-05-03 21:34:13 +0900

    setup Next-Sass

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const withSass = require('@zeit/next-sass');
+module.exports = withSass({
+    cssModules: true
+});
+
 module.exports = {
     assetPrefix: process.env.NODE_ENV === 'production' ? '/YokohamaLandscape/' : '',
 }
