a362ee0656adb1e8f9a2f0e865e344db0371f4fe
dj_yoko <yokochan.recurrent@gmail.com>
2020-03-15 00:42:01 +0900

    update docs

--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9f16350716b276a92b69";
+/******/ 	var hotCurrentHash = "7b78d36c2d157917ed93";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
