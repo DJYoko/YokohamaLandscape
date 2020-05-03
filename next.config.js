b210da79418c04003dafd4fddb81cd4aa0b84dcd
DJYoko <yokochan.recurrent@gmail.com>
2020-05-04 00:33:11 +0900

    describe specific path for Export

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
     assetPrefix: process.env.NODE_ENV === 'production' ? '/YokohamaLandscape/' : '',
+    exportPathMap: async function(
+        defaultPathMap,
+        { dev, dir, outDir, distDir, buildId }
+      ) {
+        return {
+          '/': { page: '/' },
+          '/about/index': { page: '/about' },
+          '/detail/index': { page: '/detail' },
+        }
+      },
 }
