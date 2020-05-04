9965abe4773e393b3a8c77dc29952e8df41c2414
DJYoko <yokochan.recurrent@gmail.com>
2020-05-04 11:18:44 +0900

    [refactor] apply source-code formatter on DescriptionBox component.

--- src/components/DescriptionBox.js
+++ src/components/DescriptionBox.js
@@ -1,35 +1,47 @@
-import React from 'react';
-import _JSXStyle from 'styled-jsx/style';
-import css from 'styled-jsx/css';
+import React from "react";
+import _JSXStyle from "styled-jsx/style";
+import css from "styled-jsx/css";
 
 export class DescriptionBox extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <div className="description-box">
-                <h1>{this.props.title}</h1>
-                <p>{this.props.description}<br />{this.props.annotation}</p>
-                <hr />
-                <p>Text Source<br />{this.props.authText}</p>
-                <p>Photo Source<br />{this.props.authImg}</p>
-                <style jsx>{styles}</style>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="description-box">
+        <h1>{this.props.title}</h1>
+        <p>
+          {this.props.description}
+          <br />
+          {this.props.annotation}
+        </p>
+        <hr />
+        <p>
+          Text Source
+          <br />
+          {this.props.authText}
+        </p>
+        <p>
+          Photo Source
+          <br />
+          {this.props.authImg}
+        </p>
+        <style jsx>{styles}</style>
+      </div>
+    );
+  }
 }
 
 const styles = css`
-.description-box {
+  .description-box {
     padding: 24px 24px 14px;
     background: rgba(0, 0, 0, 0.75);
     max-width: 480px;
     word-break: break-all;
-}
-h1 {
+  }
+  h1 {
     margin-top: 0;
-}
+  }
 `;
 
 export default DescriptionBox;
