b86e2c016d20b1f0a3462a05a96e351847d09384
DJYoko <yokochan.recurrent@gmail.com>
2020-05-04 11:15:46 +0900

    [refactor] apply source-code formatter on BackLink component.

--- src/components/BackLink.js
+++ src/components/BackLink.js
@@ -1,53 +1,58 @@
-import React from 'react';
-import _JSXStyle from 'styled-jsx/style';
-import css from 'styled-jsx/css';
-import Link from 'next/link';
-import CONFIG from '../../next.config';
-const ROOT = (CONFIG.assetPrefix === '') ? '/' : CONFIG.assetPrefix;
+import React from "react";
+import _JSXStyle from "styled-jsx/style";
+import css from "styled-jsx/css";
+import Link from "next/link";
+import CONFIG from "../../next.config";
+const ROOT = CONFIG.assetPrefix === "" ? "/" : CONFIG.assetPrefix;
 
 export class BackLink extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <div className="back-link-area">
-                <Link href={{
-                    pathname: ROOT
-                }}>
-                    <a className="back-link">
-                        <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                        Back
-                </a>
-                </Link>
-                <style jsx>{styles}</style>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="back-link-area">
+        <Link
+          href={{
+            pathname: ROOT,
+          }}
+        >
+          <a className="back-link">
+            <span
+              className="glyphicon glyphicon-chevron-left"
+              aria-hidden="true"
+            ></span>
+            Back
+          </a>
+        </Link>
+        <style jsx>{styles}</style>
+      </div>
+    );
+  }
 }
 
 const styles = css`
-.back-link-area {
-  display: inline-block;
-}
-.back-link {
-  color: #d9d9d9;
-  background: rgba(0, 0, 0, 0.9);
-  display: block;
-  padding: 8px;
-  text-align: center;
-  border-radius: 4px;
-}
+  .back-link-area {
+    display: inline-block;
+  }
+  .back-link {
+    color: #d9d9d9;
+    background: rgba(0, 0, 0, 0.9);
+    display: block;
+    padding: 8px;
+    text-align: center;
+    border-radius: 4px;
+  }
 
-.back-link:hover {
-  text-decoration: none;
-  color: #262626;
-  background: rgba(255, 255, 255, 0.75);
-}
+  .back-link:hover {
+    text-decoration: none;
+    color: #262626;
+    background: rgba(255, 255, 255, 0.75);
+  }
 
-.glyphicon {
-  margin-right: 4px;
-}
+  .glyphicon {
+    margin-right: 4px;
+  }
 `;
 
 export default BackLink;
