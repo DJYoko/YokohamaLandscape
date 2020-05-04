03307a5e07a74c557c66705b802e4f010fbf81ee
DJYoko <yokochan.recurrent@gmail.com>
2020-05-04 11:26:29 +0900

    [SCSS] rename selector as customized BEM, described style as scss - AreaLink components.

--- src/components/AreaLinks/index.js
+++ src/components/AreaLinks/index.js
@@ -5,7 +5,7 @@ import AreaLink from './AreaLink';
 export class AreaLinks extends React.Component {
     render() {
         return (
-            <div className="area-links row">
+            <div className="row">
                 {Areas.map((area, index) => (
                     <div className="col-xs-12 col-sm-4 col-md-3" key={index}>
                         <AreaLink title={area.title} path={area.path} key={index}></AreaLink>
