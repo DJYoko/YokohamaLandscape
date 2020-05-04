d3bba979ade76b186f2f44dd44cfe26c17ad27c4
DJYoko <yokochan.recurrent@gmail.com>
2020-05-04 11:22:09 +0900

    [SCSS] rename selector as customized BEM, described style as scss - DescriptionBox component.

--- src/components/DescriptionBox.js
+++ src/components/DescriptionBox.js
@@ -3,45 +3,45 @@ import _JSXStyle from "styled-jsx/style";
 import css from "styled-jsx/css";
 
 export class DescriptionBox extends React.Component {
-  constructor(props) {
+    constructor(props) {
     super(props);
-  }
-  render() {
-    return (
-      <div className="description-box">
-        <h1>{this.props.title}</h1>
-        <p>
-          {this.props.description}
-          <br />
-          {this.props.annotation}
-        </p>
-        <hr />
-        <p>
-          Text Source
-          <br />
-          {this.props.authText}
-        </p>
-        <p>
-          Photo Source
-          <br />
-          {this.props.authImg}
-        </p>
-        <style jsx>{styles}</style>
-      </div>
-    );
-  }
+    }
+    render() {
+        return (
+            <div className="u-descriptionBox">
+                <h1>{this.props.title}</h1>
+                <p>
+                    {this.props.description}
+                    <br />
+                    {this.props.annotation}
+                </p>
+                <hr />
+                <p>
+                    Text Source
+                    <br />
+                    {this.props.authText}
+                </p>
+                <p>
+                    Photo Source
+                    <br />
+                    {this.props.authImg}
+                </p>
+                <style jsx>{styles}</style>
+            </div>
+        );
+    }
 }
 
 const styles = css`
-  .description-box {
-    padding: 24px 24px 14px;
-    background: rgba(0, 0, 0, 0.75);
-    max-width: 480px;
-    word-break: break-all;
-  }
-  h1 {
-    margin-top: 0;
-  }
+    .u-descriptionBox {
+        padding: 24px 24px 14px;
+        background: rgba(0, 0, 0, 0.75);
+        max-width: 480px;
+        word-break: break-all;
+        h1 {
+            margin-top: 0;
+        }
+    }
 `;
 
 export default DescriptionBox;
