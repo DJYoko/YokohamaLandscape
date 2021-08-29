065ebcbf8c9511e1e1c509e7692e303b79b4a056
DJYoko <yokochan.recurrent@gmail.com>
2021-08-29 22:22:43 +0900

    import assert prefix into route path generator

--- src/functions/generateSiteRoute.js
+++ src/functions/generateSiteRoute.js
@@ -1,6 +1,9 @@
+import CONFIG from '../../next.config';
+const ROOT = CONFIG.assetPrefix === '' ? '/' : CONFIG.assetPrefix;
+
 export default function generateSiteRoute(originalPath) {
-    if (process.env.NODE_ENV === "production") {
-        return "/YokohamaLandscape/" + originalPath.replace(/^\//g, "");
-    }
-    return originalPath;
+  if (process.env.NODE_ENV === 'production') {
+    return ROOT + originalPath.replace(/^\//g, '');
+  }
+  return originalPath;
 }
