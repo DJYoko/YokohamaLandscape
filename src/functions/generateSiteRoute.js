fd8a9d4741b8b2a9431d8556dbdf8c3050127664
DJYoko <yokochan.recurrent@gmail.com>
2021-08-29 21:52:19 +0900

    add function for generate path (dev, prod compatible)

---
+++ src/functions/generateSiteRoute.js
@@ -0,0 +1,6 @@
+export default function generateSiteRoute(originalPath) {
+  if (process.env.NODE_ENV === 'production') {
+    return '/YokohamaLandscape/' + originalPath.replace(/^\//g, '')
+  }
+  return originalPath
+}
